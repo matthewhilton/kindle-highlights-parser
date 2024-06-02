171e7126ecf47b5458a147fe2c9da3c7bc2e70cd
Matthew Hilton <mj.hilton@outlook.com>
2024-06-02 18:44:49 +1000

    chore: add vercel export adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    integrations: [react(), tailwind(), icon()],
+  output: 'server',
+  integrations: [react(), tailwind(), icon()],
+  adapter: vercel()
 });
